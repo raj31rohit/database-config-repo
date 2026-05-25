01c5d20207214644aa3617155b32e75796f4fa54
raj31rohit <raj31.rohit.com>
2026-05-25 14:34:00 +0530

    added new table

--- database/app_schema/tables/payments.sql
+++ database/app_schema/tables/payments.sql
@@ -1,11 +1,5 @@
 CREATE TABLE payments (
-
     payment_id NUMBER PRIMARY KEY,
-
     order_id NUMBER NOT NULL,
-
-    payment_amount NUMBER(10,2),
-
-    payment_date DATE DEFAULT SYSDATE
-
+    payment_amount NUMBER(10,2)
 );
