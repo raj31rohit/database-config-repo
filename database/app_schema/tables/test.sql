2789fe7bb7811ceff9efdc34b0e7be448cf1b707
raj31rohit <raj31.rohit.com>
2026-05-31 17:07:07 +0530

    added new table appconfi3

---
+++ database/app_schema/tables/test.sql
@@ -0,0 +1,8 @@
+CREATE TABLE customer_address2 (
+    address_id NUMBER PRIMARY KEY,
+    customer_id NUMBER NOT NULL,
+    city VARCHAR2(100),
+    state VARCHAR2(100),
+    country VARCHAR2(100),
+    created_date DATE DEFAULT SYSDATE
+);
