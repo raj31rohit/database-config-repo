7f396040bcf454c5556bbbb7be78afdf8a15fcd9
raj31rohit <raj31.rohit.com>
2026-06-01 21:54:37 +0530

    test: add payments table and change script with rollback (should PASS)

---
+++ database/app_schema/changes/add_note_to_payments.sql
@@ -0,0 +1,1 @@
+﻿ALTER TABLE payments ADD note VARCHAR2(500);
---
+++ database/app_schema/tables/payments.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE payments (
+    payment_id  NUMBER(10)    NOT NULL,
+    order_id    NUMBER(10)    NOT NULL,
+    amount      NUMBER(12,2)  NOT NULL,
+    paid_on     DATE          DEFAULT SYSDATE,
+    CONSTRAINT pk_payments PRIMARY KEY (payment_id)
+);
